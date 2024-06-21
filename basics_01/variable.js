0814affe01947947f7abec9a2094ad9b6545167e
Aditiiojha <ojhaaditihehe@gmail.com>
2024-06-21 03:15:09 +0000

    variables(const , let and var)

---
+++ basics_01/variable.js
@@ -0,0 +1,8 @@
+const emailid="aditi@gmail.com";
+// after using const we can't change the variables value.
+let address="jamshedpur";
+// we prefer let over var , as var does not have scope block. i.e. if we use "i" in to for loops then one of the loop will affect the other.
+ var id="1234";
+//  not prefered
+console.table([address,emailid,id]);
+// console.table is used to print several values at the same time . without using console.log repetively.
