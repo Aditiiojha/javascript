98df076422a59ecdebcff0b1dd99bf6acd11c2a2
Aditi kumari <ojhaaditihehe@gmail.com>
2024-06-21 06:51:39 +0000

    operations in js

---
+++ datatypes/operations.js
@@ -0,0 +1,12 @@
+// console.log(2**2); will be 2 raise to the power 2
+console.log("1"+12+1);
+// since the 1st input is string type therefore the result will be of string type aswell . (result=1121)
+console.log(1+12+"1");
+// since the 1st input is number type therefore the result will be of number aswell (result=131)
+console.log(typeof ("1"+12+1) );
+console.log( typeof (1+12+"1"));
+// the type of the result will be string 
+/*For 1 + 12 + "1":
+
+1 + 12 results in 13 (number).
+13 + "1" results in "131" (string). */
