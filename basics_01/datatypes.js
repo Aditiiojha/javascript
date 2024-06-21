2220b4c93cb4ba20bf83ef9bfa84d33db89c521f
Aditi kumari <ojhaaditihehe@gmail.com>
2024-06-21 03:45:53 +0000

    datatypes

---
+++ basics_01/datatypes.js
@@ -0,0 +1,14 @@
+"use strict";
+// treats all the file as the newer version
+console.log(3+2); console.log("aditi");
+// Data types
+ let name="aditi";    
+//  string 
+let na; 
+// data type will be undefined , as we have declared the value but did not assign its value
+ // null => it is a standalone value , empty 
+//  boolean=> true or false
+// number , bigInt
+// symbol => in react we use symbol to find the uniqueness of the componets
+console.log(typeof "aditi");
+// typeof => null will be object , whereas the typeof undefined would be undefined.
