19109d5d1628629cb6b9c757969c46c3a7ca5712
Aditi kumari <ojhaaditihehe@gmail.com>
2024-06-21 07:51:42 +0000

    comparision of datatypes

---
+++ datatypes/comparision.js
@@ -0,0 +1,4 @@
+// comparision should always be done on the same data type , as sometimes they might give different result from the predicted one.
+console.log("2">1);
+// here the value will be true , "2 " will be converted into number and then will be checked , but this might not be the case everytime.
+//  this confusion might occur with "null" and "undefined"
